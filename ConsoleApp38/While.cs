ed2a1ba9b7a0bfcf3258d1aa62b9da9a77c41e90
Romeo Cosma <122970841+RoMeRO93@users.noreply.github.com>
2023-05-23 15:26:35 +0300

    Update While.cs

--- ConsoleApp38/While.cs
+++ ConsoleApp38/While.cs
@@ -1,11 +1,13 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Bun venit în aplicatia de management al angajatilor!");
-        // Introducerea informațiilor personale
+        Console.WriteLine("Bun venit in aplicatia de management al angajatilor!");
+
         Console.WriteLine("Introduceti numele:");
         string nume = Console.ReadLine();
 
@@ -15,20 +17,16 @@ internal class Program
         Console.WriteLine("Introduceti nivelul de educatie:");
         string educatie = Console.ReadLine();
 
-        // Verificarea eligibilității pentru angajare în IT
-        bool isEligibilIT = VerificareEligibilitateIT(varsta, educatie);
+        bool isEligibilIT = AngajatManager.VerificareEligibilitateIT(varsta, educatie);
 
-        // Adăugarea angajatului în sistem
         Angajat angajat = new Angajat(nume, varsta, educatie, isEligibilIT);
         AngajatManager.AdaugaAngajat(angajat);
 
-        Console.WriteLine("Angajatul a fost adăugat cu succes in sistem!");
+        Console.WriteLine("Angajatul a fost adaugat cu succes in sistem!");
 
-        // Afisarea listei de angajati
         Console.WriteLine("\nLista de angajati:");
         AngajatManager.AfiseazaAngajati();
 
-        // Căutarea unui angajat după nume
         Console.WriteLine("\nIntroduceti numele angajatului cautat:");
         string numeCautat = Console.ReadLine();
         Angajat angajatCautat = AngajatManager.CautaAngajatDupaNume(numeCautat);
@@ -41,16 +39,17 @@ internal class Program
         {
             Console.WriteLine("Angajatul cautat nu a fost gasit in sistem.");
         }
-    }
 
-    private static bool VerificareEligibilitateIT(int varsta, string educatie)
-    {
-        if (varsta >= 18 && educatie.ToLower() == "superior")
-        {
-            return true;
-        }
+        Console.WriteLine("\nIntroduceti numele angajatului de sters:");
+        string numeSters = Console.ReadLine();
+        AngajatManager.StergeAngajat(numeSters);
 
-        return false;
+        Console.WriteLine("\nIntroduceti numele angajatului de actualizat:");
+        string numeActualizat = Console.ReadLine();
+        AngajatManager.ActualizeazaAngajat(numeActualizat);
+
+        int numarAngajati = AngajatManager.NumarAngajati();
+        Console.WriteLine($"\nNumarul total de angajati: {numarAngajati}");
     }
 }
 
@@ -60,6 +59,7 @@ internal class Angajat
     public int Varsta { get; set; }
     public string Educatie { get; set; }
     public bool IsEligibilIT { get; set; }
+
     public Angajat(string nume, int varsta, string educatie, bool isEligibilIT)
     {
         Nume = nume;
@@ -72,6 +72,7 @@ internal class Angajat
 internal class AngajatManager
 {
     private static List<Angajat> angajati = new List<Angajat>();
+
     public static void AdaugaAngajat(Angajat angajat)
     {
         angajati.Add(angajat);
@@ -88,5 +89,60 @@ internal class AngajatManager
     public static Angajat CautaAngajatDupaNume(string nume)
     {
         return angajati.FirstOrDefault(angajat => angajat.Nume.ToLower() == nume.ToLower());
+    }
+
+    public static void StergeAngajat(string nume)
+    {
+        Angajat angajat = CautaAngajatDupaNume(nume);
+        if (angajat != null)
+        {
+            angajati.Remove(angajat);
+            Console.WriteLine($"Angajatul cu numele {angajat.Nume} a fost sters din sistem.");
+        }
+        else
+        {
+            Console.WriteLine("Angajatul cautat nu a fost gasit in sistem.");
+        }
+    }
+
+    public static void ActualizeazaAngajat(string nume)
+    {
+        Angajat angajat = CautaAngajatDupaNume(nume);
+        if (angajat != null)
+        {
+            Console.WriteLine("Introduceti noile informatii:");
+
+            Console.WriteLine("Introduceti noul nume:");
+            angajat.Nume = Console.ReadLine();
+
+            Console.WriteLine("Introduceti noua varsta:");
+            angajat.Varsta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti noul nivel de educatie:");
+            angajat.Educatie = Console.ReadLine();
+
+            angajat.IsEligibilIT = VerificareEligibilitateIT(angajat.Varsta, angajat.Educatie);
+
+            Console.WriteLine("Angajatul a fost actualizat cu succes!");
+        }
+        else
+        {
+            Console.WriteLine("Angajatul cautat nu a fost gasit in sistem.");
+        }
+    }
+
+    public static int NumarAngajati()
+    {
+        return angajati.Count;
+    }
+
+    public static bool VerificareEligibilitateIT(int varsta, string educatie)
+    {
+        if (varsta >= 18 && educatie.ToLower() == "superior")
+        {
+            return true;
+        }
+
+        return false;
     }
 }
